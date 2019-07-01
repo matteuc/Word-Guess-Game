05dbc05f5da94906cf1d745223b25f567758144f
matteuc <ymc006@ucsd.edu>
2019-07-01 09:04:12 -0700

    fixed some bugs, runs well for the most part

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,14 +15,32 @@ function updateGuesses() {
     document.getElementById('guess-counter').innerHTML = guesses;
 }
 
+function displayMessage(curr_word, win) {
+    var elem = document.getElementById('instructions');
+    var word = document.getElementById('blanks');
+    if (win) {
+        elem.innerHTML = "You won!\n\nPress any key to play again.";
+    } else {
+        elem.innerHTML = "You lost!\n\nPress any key to play again";
+        word.innerHTML = curr_word;
+        num_blanks = 0;
+    }
+}
+
 // Print the appropriate number of blanks for the current word
 function printBlanks(word) {
     var elem = document.getElementById('blanks');
     elem.innerHTML = "";
 
     for (var i = 0; i < word.length; i++) {
-        elem.innerHTML += "__";
-        !(i === word.length - 1) ? elem.innerHTML += ' ': ''
+        if (word[i] === ' ') {
+            elem.innerHTML += "\xa0\xa0\xa0";
+            num_blanks--;
+        } else {
+            elem.innerHTML += "__";
+        }
+        
+        !(i === word.length - 1) ? elem.innerHTML += '\xa0': ''
     }
 
 }
@@ -32,17 +50,18 @@ function revealLetters(word, letter) {
     elem.innerHTML = "";
     revealed.push(letter);
 
-
     for (var i = 0; i < word.length; i++) {
-        if (revealed.includes(word[i])) {
-            elem.innerHTML += word[i]; 
-            word[i] === letter ? num_blanks-- :'';
-        }
-        else {
+        if (revealed.includes(word[i]) && word[i] !== " ") {
+            elem.innerHTML += word[i];
+            word[i] === letter ? num_blanks-- : '';
+        } else if (word[i] === " ") {
+            console.log("test");
+            elem.innerHTML += "\xa0";
+        } else {
             elem.innerHTML += "__";
         }
 
-        !(i === word.length - 1) ? elem.innerHTML += ' ': ''
+        !(i === word.length - 1) ? elem.innerHTML += '\xa0': ''
     }
 
 }
@@ -64,14 +83,18 @@ function newRandomWord(words) {
     return words[Math.floor(Math.random() * words.length)];
 }
 
-var puppy_words = ["corgi", "pug"];
+function changeImage(newSrc) {
+    document.getElementById('word-image').src = newSrc;
+}
+
+var puppy_words = ["corgi", "pug", "beagle", "husky", "chow chow", "golden retriever", "poodle", "french bulldog", "border collie"];
 
 document.onkeyup = function (event) {
     // Set a new word if one does not already exist or all blanks have been filled
     if (curr_word === "" || num_blanks === 0) {
         curr_word = newRandomWord(puppy_words);
-        printBlanks(curr_word);
         num_blanks = curr_word.length;
+        printBlanks(curr_word);
         guesses = curr_word.length * 2;
         updateGuesses();
         revealed = [];
@@ -81,14 +104,18 @@ document.onkeyup = function (event) {
 
     var letter = event.key.toLowerCase();
     // Reveal letters if entered key is within current word
-    (curr_word.includes(letter) && !revealed.includes(letter)) ? revealLetters(curr_word, letter): (!bank.includes(letter) && !revealed.includes(letter)) ? addToBank(letter) : '' ;
+    (curr_word.includes(letter) && !revealed.includes(letter)) ? revealLetters(curr_word, letter): (!bank.includes(letter) && !revealed.includes(letter)) ? addToBank(letter) : '';
 
 
     if (num_blanks === 0 && guesses !== 0) {
         //Display Victory Messaage
+        displayMessage(curr_word, true);
         updateWins();
+        changeImage("assets/images/" + curr_word + ".jpg");
     } else if (guesses === 0) {
         //Display Defeat Message
+        displayMessage(curr_word, false);
+        changeImage("assets/images/" + curr_word + ".jpg");
     }
 
 
